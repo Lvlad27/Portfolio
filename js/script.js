a806b00234b3b6b9e3561d23c517b9db1b82d139
Vlad Leontescu <Lvlad27@gmail.com>
2021-12-15 15:15:37 +0200

    navbar dropdown & animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+'use strict';
 
+const navbar = document.querySelector('.header__container-navbar');
+const menuToggle = document.querySelector('.header__container-menuToggle');
+const navLinks = document.querySelectorAll('.navbar-links');
+
+menuToggle.addEventListener('click', function () {
+	navbar.classList.toggle('header__container-navbar--open');
+	navLinks.forEach(element => {
+		element.classList.toggle('navbar-links--open');
+	});
+});
