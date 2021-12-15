cf54edfbe5f92d7ae711f7473225cfb175321d77
Vlad Leontescu <Lvlad27@gmail.com>
2021-12-15 23:08:25 +0200

    added blur effect on menu dropdown

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 'use strict';
 
+////////////////////////////
+// MOBILE NAVBAR
 const navbar = document.querySelector('.header__container__navbar');
 const menuToggle = document.querySelector('.header__container-menuToggle');
-const navLinks = document.querySelectorAll('header__container__navbar-links');
+const navLinks = document.querySelectorAll('.header__container__navbar-links');
+const overlay = document.querySelector('.header__overlay');
+const noScroll = document.querySelector('.noScroll');
 
 menuToggle.addEventListener('click', function () {
 	navbar.classList.toggle('header__container__navbar--open');
 	navLinks.forEach(element => {
 		element.classList.toggle('header__container__navbar-links--open');
 	});
+	this.classList.toggle('header__container-menuToggle--open');
+	overlay.classList.toggle('header__overlay--hidden');
+	noScroll.classList.toggle('noScroll');
 });
