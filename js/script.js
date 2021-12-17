1a102ce66ee9480a230dbd77b61509d646ac0fc7
Vlad Leontescu <Lvlad27@gmail.com>
2021-12-17 15:31:32 +0200

    updated

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const navbar = document.querySelector('.header__container__navbar');
 const menuToggle = document.querySelector('.header__container-menuToggle');
 const navLinks = document.querySelectorAll('.header__container__navbar-links');
 const overlay = document.querySelector('.header__overlay');
+const body = document.querySelector('body');
 
 menuToggle.addEventListener('click', function () {
 	navbar.classList.toggle('header__container__navbar--open');
@@ -14,5 +15,5 @@ menuToggle.addEventListener('click', function () {
 	});
 	this.classList.toggle('header__container-menuToggle--open');
 	overlay.classList.toggle('header__overlay--hidden');
-	document.querySelector('body').style.overflow = 'hidden';
+	body.classList.toggle('noScroll');
 });
