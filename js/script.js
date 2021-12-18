2175da53a25ebcb345efc562919a5c9a876c5298
Vlad Leontescu <Lvlad27@gmail.com>
2021-12-18 03:29:22 +0200

    added scroll effect for menu bar for mobile (wip)

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const navbar = document.querySelector('.header__container__navbar');
 const menuToggle = document.querySelector('.header__container-menuToggle');
 const navLinks = document.querySelectorAll('.header__container__navbar-links');
 const overlay = document.querySelector('.header__overlay');
-const body = document.querySelector('body');
+const body = document.body;
 
 menuToggle.addEventListener('click', function () {
 	navbar.classList.toggle('header__container__navbar--open');
@@ -16,4 +16,26 @@ menuToggle.addEventListener('click', function () {
 	this.classList.toggle('header__container-menuToggle--open');
 	overlay.classList.toggle('header__overlay--hidden');
 	body.classList.toggle('noScroll');
+});
+
+let lastScroll = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+	const currentScroll = window.pageYOffset;
+	if (currentScroll <= 0) {
+		body.classList.remove('scroll-up');
+		header.classList.remove('header__scroll');
+		return;
+	}
+
+	if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+		body.classList.remove('scroll-up');
+		body.classList.add('scroll-down');
+		header.classList.add('header__scroll');
+	} else if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+		body.classList.remove('scroll-down');
+		body.classList.add('scroll-up');
+	}
+	lastScroll = currentScroll;
 });
