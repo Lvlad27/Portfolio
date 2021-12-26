d02fded7de88bd186de9691d98cf78d479006ad2
Vlad Leontescu <Lvlad27@gmail.com>
2021-12-26 11:17:06 +0200

    startetd modal feature to porftolio section

--- js/script.js
+++ js/script.js
@@ -64,3 +64,30 @@ category.addEventListener('click', function (e) {
 		});
 	}
 });
+
+// PORTFOLIO MODAL
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.open-modal');
+console.log(btnsOpenModal);
+
+const openModal = function () {
+	modal.classList.remove('modal--hidden');
+};
+
+const closeModal = function () {
+	modal.classList.add('modal--hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+	console.log(e.key);
+
+	if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+		closeModal();
+	}
+});
